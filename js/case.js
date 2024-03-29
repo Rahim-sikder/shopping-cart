4e8c37b7ddb5ddf2f287ddf58b6c391050d1175c
Rahim sikder <arsikder323@gmail.com>
2024-03-29 11:29:33 +0600

    iphone 11 number plus minus increase complete

--- js/case.js
+++ js/case.js
@@ -1,33 +1,18 @@
 
-function updateCaseNumber(isIncrease){
-   const caseNumberField = document.getElementById('case-number-field');
-   const caseNumberString =caseNumberField.value;
-   const previousCaseNumber = parseInt(caseNumberString);
-
-   if(isIncrease == true){
-       newCaseNumber = previousCaseNumber + 1;
-   }
-   else{
-      newCaseNumber = previousCaseNumber - 1;
-   }
-
-   
-   caseNumberField.value = newCaseNumber;
+function updateCaseTotalPrice (newCaseNumber){
+      
+   const caseTotalPrice = newCaseNumber * 56;
+   const caseTotalaElement = document.getElementById('case-total');
+   caseTotalaElement.innerText = caseTotalPrice;
 }
-
 
 
 document.getElementById('btn-case-plus').addEventListener('click',function(){
         updateCaseNumber(true);
-
-        const caseTotalPrice = newCaseNumber * 56;
-        const caseTotalaElement = document.getElementById('case-total');
-        caseTotalaElement.innerText = caseTotalPrice;
+        updateCaseTotalPrice (newCaseNumber)
 });
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
       updateCaseNumber(false);
-      const caseTotalPrice = newCaseNumber * 56;
-        const caseTotalaElement = document.getElementById('case-total');
-        caseTotalaElement.innerText = caseTotalPrice;
+      updateCaseTotalPrice (newCaseNumber)
   });
---
+++ js/common.js
@@ -0,0 +1,17 @@
+function updateCaseNumber(isIncrease){
+   const caseNumberField = document.getElementById('case-number-field');
+   const caseNumberString =caseNumberField.value;
+   const previousCaseNumber = parseInt(caseNumberString);
+
+   let newCaseNumber;
+   if(isIncrease == true){
+       newCaseNumber = previousCaseNumber + 1;
+   }
+   else{
+      newCaseNumber = previousCaseNumber - 1;
+   }
+
+   
+   caseNumberField.value = newCaseNumber;
+   return newCaseNumber
+}
